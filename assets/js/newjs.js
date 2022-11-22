ce0eb9f40ea48a3baa27de2d9cecb07b19221271
LeLwy <Louis.Chancioux@gmail.com>
2022-11-22 15:01:47 +0100

    Encore du JS

---
+++ assets/js/newjs.js
@@ -0,0 +1,10 @@
+//Je trouve mon formulaire
+////(Le formulaire est considéré valide par défaut)
+
+//Lorsque que je le soummet, un évènement se produit
+
+//Je vérifie le premier input (#userName). Si il est vide, le formulaire est invalidé et un message d'erreur apparait
+
+//Je vérifie le second input (#email). Si il n'a pas le format d'un email, le formulaire est invalidé et un message d'erreur apparait
+
+//Je vérifie le quatrième input (#confirmPassword). Si il est vide, le formulaire est invalidé et un message d'erreur apparait
--- assets/js/script.js
+++ assets/js/script.js
@@ -13,58 +13,55 @@ let intCenterForm = document.getElementById("intCenter");
 
 // RECUPERATION DES INPUTS PASSWORD
 
+let connectInfosFormOK = true;
 
 connectInfosForm.addEventListener("submit", function(e){
-    
+
     e.preventDefault(); // stopper la soumission auto
 
-    let connectInfosInputs = document.querySelectorAll("body > main > section > #connectInfos > fieldset > input")
+    let userName = document.querySelector("body > main > section > #connectInfos > fieldset > #userName");
     
-    //let connectInfosFormOK = true;
-
-    for (let i = 0; i < connectInfosInputs.length; i++){
+    if(userName.value === ""){
         
-        if(connectInfosInputs[i].hasAttribute("required") !== true && connectInfosInputs[i].value() === ""){
-
-            let userName = document.querySelector("body > main > section > #connectInfos > fieldset > #userName");
-            
-            if(userName.value() === ""){
-                
-                let userNameFieldset = document.querySelector("body > main > section > #connectInfos > fieldset:nth-child(1)");
-                let userNameError = document.createElement("p");
-                userNameError.innerHTML= "Veuillez choisir un nom d'utilisateur";
-                userNameError.classList.add("error-displayed");
-                userNameFieldset.appendChild(userNameError)
-            }
-
-            let email = document.querySelector("body > main > section > #connectInfos > fieldset > #email");
-
-            if(!email.match(/^\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,3}$/)){
-
-                let emailFieldset = document.querySelector("body > main > section > #connectInfos > fieldset:nth-child(2)");
-                let emailError = document.createElement("p");
-                emailError.innerHTML= "Veuillez choisir un email valide";
-                emailError.classList.add("error-displayed");
-                emailFieldset.appendChild(emailError);
-            }
-
-            let password = document.querySelector("body > main > section > #connectInfos > fieldset > #password");
-            let confirmPassword = document.querySelector("body > main > section > #connectInfos > fieldset > #confirm-password");
-
-            if(password !== confirmPassword){
-
-                let passwordFieldset = document.querySelector("body > main > section > #connectInfos > fieldset:nth-child(4)");
-                let passwordError = document.createElement("p");
-                passwordError.innerHTML= "Les mots de passe ne corespondent pas";
-                passwordError.classList.add("error-displayed");
-                passwordFieldset.appendChild(passwordError);
-            }
-
-        }else{
-            
-            e.target.submit(); // le soumettre Ã  la main
-        }
+        let userNameFieldset = document.querySelector("body > main > section > #connectInfos > fieldset:nth-child(1)");
+        let userNameError = document.createElement("p");
+        userName.classList.add("red-border");
+        userNameError.innerHTML= "Veuillez choisir un nom d'utilisateur";
+        userNameError.classList.add("error-displayed");
+        userNameFieldset.appendChild(userNameError)
+        connectInfosFormOK = false;
+        console.log(connectInfosFormOK)
     }
+
+    let email = document.querySelector("body > main > section > #connectInfos > fieldset > #email");
+
+    if((email.value === "") && (email.value.indexOf("@", 0) < 0) && (email.value.indexOf(".", 0) < 0)){
+
+        let emailFieldset = document.querySelector("body > main > section > #connectInfos > fieldset:nth-child(2)");
+        let emailError = document.createElement("p");
+        emailError.innerHTML= "Veuillez choisir un email valide";
+        emailError.classList.add("error-displayed");
+        emailFieldset.appendChild(emailError);
+        connectInfosFormOK = false;
+    }  
+
+    let password = document.querySelector("body > main > section > #connectInfos > fieldset > #password");
+    let confirmPassword = document.querySelector("body > main > section > #connectInfos > fieldset > #confirm-password");
+
+    if(password.value !== confirmPassword.value){
+
+        let passwordFieldset = document.querySelector("body > main > section > #connectInfos > fieldset:nth-child(4)");
+        let passwordError = document.createElement("p");
+        passwordError.innerHTML= "Les mots de passe ne corespondent pas";
+        passwordError.classList.add("error-displayed");
+        passwordFieldset.appendChild(passwordError);
+        connectInfosFormOK = false;
+    }
+
+    // if(connectInfosFormOK = true){
+
+    //     e.target.submit(); // le soumettre Ã  la main
+    // }
 })
 
 
